3abd19557e3c27640790b76ded6c1feb7b6c49c4
Max Whale <maxwhale99@gmail.com>
2020-01-23 22:42:52 +0000

    Add mouse-look and other improvements

--- Labs/starfield/Source/skeleton.cpp
+++ Labs/starfield/Source/skeleton.cpp
@@ -9,17 +9,26 @@ using namespace std;
 using glm::mat3;
 using glm::vec3;
 
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
 #define SCREEN_WIDTH 320
 #define SCREEN_HEIGHT 256
 #define CAM_VELOCITY 0.4f
 #define STAR_VELOCITY 0.5f
+#define MOUSE_SENSITIVITY 0.0015f;
 #define FULLSCREEN_MODE false
 
 /* ----------------------------------------------------------------------------*/
 /* GLOBAL VARIABLES                                                            */
 int t;
 vec3 camera_pos;
-vector<vec3> stars(1000);
+vec3 camera_rot;
+vec3 cam_forward;
+vec3 cam_right;
+mat3 R;
+vector<vec3> stars(100000);
 
 /* ----------------------------------------------------------------------------*/
 /* FUNCTIONS                                                                   */
@@ -27,17 +36,27 @@ void Update();
 void Draw(screen *screen);
 void Interpolate(float a, float b, vector<float> &result);
 void Interpolate(vec3 a, vec3 b, vector<vec3> &result);
+void HandleKeyboard(const Uint8 *state, float dt);
+void HandleMouse();
+void UpdateStars(float dt);
+void ClampCamera(vec3 &rot);
+void CalcRMatrix(vec3 rot, mat3 &R);
 
 int main(int argc, char *argv[]) {
 
   screen *screen = InitializeSDL(SCREEN_WIDTH, SCREEN_HEIGHT, FULLSCREEN_MODE);
+  SDL_SetRelativeMouseMode(SDL_TRUE);
   t = SDL_GetTicks(); /*Set start value for timer.*/
   for (size_t s = 0; s < stars.size(); ++s) {
     stars[s].x = float(rand()) * 2.0f / float(RAND_MAX) - 1.0f;
     stars[s].y = float(rand()) * 2.0f / float(RAND_MAX) - 1.0f;
     stars[s].z = float(rand()) / float(RAND_MAX);
   }
-  camera_pos = vec3(0,0,0);
+  camera_pos = vec3(0, 0, 0);
+  camera_rot = vec3(0, 0, 0);
+  CalcRMatrix(camera_rot, R);
+  cam_forward = glm::transpose(R) * vec3(0, 0, 1);
+  cam_right = glm::transpose(R) * vec3(1, 0, 0);
 
   while (NoQuitMessageSDL()) {
     Draw(screen);
@@ -57,14 +76,12 @@ void Draw(screen *screen) {
   memset(screen->buffer, 0, screen->height * screen->width * sizeof(uint32_t));
   for (size_t s = 0; s < stars.size(); ++s) {
     float f = SCREEN_HEIGHT / 2.0f;
-    float z = stars[s].z - camera_pos.z;
-    if (z <= 0) continue;
-    float x = stars[s].x - camera_pos.x;
-    float y = stars[s].y - camera_pos.y;
-    int px = f * x / z + SCREEN_WIDTH / 2;
-    int py = f * y / z + SCREEN_HEIGHT / 2;
+    vec3 pos = R * (stars[s] - camera_pos);
+    if (pos.z <= 0) continue;
+    int px = f * pos.x / pos.z + SCREEN_WIDTH / 2;
+    int py = f * pos.y / pos.z + SCREEN_HEIGHT / 2;
     if (px > 0 && px < SCREEN_WIDTH && py > 0 && py < SCREEN_HEIGHT) {
-      vec3 colour = 0.2f * vec3(1, 1, 1) / (z * z);
+      vec3 colour = 0.2f * vec3(1, 1, 1) / (pos.z * pos.z);
       PutPixelSDL(screen, px, py, colour);
     }
   }
@@ -74,38 +91,14 @@ void Draw(screen *screen) {
 void Update() {
   /* Compute frame time */
   int t2 = SDL_GetTicks();
-  float dt = float(t2 - t);
+  float dt = float(t2 - t) / 1000.0f;
   t = t2;
   /*Good idea to remove this*/
-  std::cout << "Render time: " << dt << " ms." << std::endl;
+  std::cout << "Render time: " << dt * 1000 << " ms." << std::endl;
   /* Update variables*/
-  const Uint8 *state = SDL_GetKeyboardState(NULL);
-  float ds = dt / 1000.0f * CAM_VELOCITY;
-  if (state[SDL_SCANCODE_A]) {
-    camera_pos.x -= ds;
-  }
-  if (state[SDL_SCANCODE_D]) {
-    camera_pos.x += ds;
-  }
-  if (state[SDL_SCANCODE_W]) {
-    camera_pos.z += ds;
-  }
-  if (state[SDL_SCANCODE_S]) {
-    camera_pos.z -= ds;
-  }
-  if (state[SDL_SCANCODE_LSHIFT]) {
-    camera_pos.y += ds;
-  }
-  if (state[SDL_SCANCODE_SPACE]) {
-    camera_pos.y -= ds;
-  }
-  for (size_t s = 0; s < stars.size(); ++s) {
-    stars[s].z += dt / 1000.0f * STAR_VELOCITY;
-    if (stars[s].z <= 0)
-      stars[s].z += 1;
-    if (stars[s].z > 1)
-      stars[s].z -= 1;
-  }
+  HandleKeyboard(SDL_GetKeyboardState(NULL), dt);
+  HandleMouse();
+  UpdateStars(dt);
 }
 
 void Interpolate(float a, float b, vector<float> &result) {
@@ -137,3 +130,72 @@ void Interpolate(vec3 a, vec3 b, vector<vec3> &result) {
     a += step;
   }
 }
+
+void HandleKeyboard(const Uint8 *state, float dt) {
+  float ds = dt * CAM_VELOCITY;
+  if (state[SDL_SCANCODE_LSHIFT])
+    ds *= 2;
+  if (state[SDL_SCANCODE_A])
+    camera_pos -= cam_right * ds;
+  if (state[SDL_SCANCODE_D])
+    camera_pos += cam_right * ds;
+  if (state[SDL_SCANCODE_W])
+    camera_pos += cam_forward * ds;
+  if (state[SDL_SCANCODE_S])
+    camera_pos -= cam_forward * ds;
+  if (state[SDL_SCANCODE_LCTRL])
+    camera_pos.y += ds;
+  if (state[SDL_SCANCODE_SPACE])
+    camera_pos.y -= ds;
+}
+
+void HandleMouse() {
+  int motion_x = 0;
+  int motion_y = 0;
+  SDL_GetRelativeMouseState(&motion_x, &motion_y);
+  if (motion_x + motion_y) {
+    camera_rot.y += motion_x * MOUSE_SENSITIVITY;
+    camera_rot.x -= motion_y * MOUSE_SENSITIVITY;
+    ClampCamera(camera_rot);
+    CalcRMatrix(camera_rot, R);
+    cam_forward = glm::transpose(R) * vec3(0, 0, 1);
+    cam_right = glm::transpose(R) * vec3(1, 0, 0);
+  }
+}
+
+void UpdateStars(float dt) {
+  for (size_t s = 0; s < stars.size(); ++s) {
+    stars[s].z += dt * STAR_VELOCITY;
+    if (stars[s].z <= 0) {
+      stars[s].z += 1;
+      stars[s].x = float(rand()) * 2.0f / float(RAND_MAX) - 1.0f;
+      stars[s].y = float(rand()) * 2.0f / float(RAND_MAX) - 1.0f;
+    }
+    if (stars[s].z > 1) {
+      stars[s].z -= 1;
+      stars[s].x = float(rand()) * 2.0f / float(RAND_MAX) - 1.0f;
+      stars[s].y = float(rand()) * 2.0f / float(RAND_MAX) - 1.0f;
+    }
+  }
+}
+
+void ClampCamera(vec3 &rot) {
+  if (camera_rot.x > M_PI / 2)
+    camera_rot.x = M_PI / 2;
+  if (camera_rot.x < -M_PI / 2)
+    camera_rot.x = -M_PI / 2;
+  if (camera_rot.y > M_PI)
+    camera_rot.y -= 2 * M_PI;
+  if (camera_rot.y < M_PI)
+    camera_rot.y += 2 * M_PI;
+}
+
+void CalcRMatrix(vec3 rot, mat3 &R) {
+  float sin_x = sinf(rot.x);
+  float cos_x = cosf(rot.x);
+  float sin_y = sinf(rot.y);
+  float cos_y = cosf(rot.y);
+  mat3 Rx = mat3(1, 0, 0, 0, cos_x, -sin_x, 0, sin_x, cos_x);
+  mat3 Ry = mat3(cos_y, 0, sin_y, 0, 1, 0, -sin_y, 0, cos_y);
+  R = Rx * Ry;
+};
