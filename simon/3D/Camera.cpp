b777f592f3676845d1b19b1abcf553e8e0164053
Max Whale <maxwhale99@gmail.com>
2020-03-21 15:45:53 +0000

    Fix projection bug

--- simon/3D/Camera.cpp
+++ simon/3D/Camera.cpp
@@ -15,7 +15,7 @@ void Camera::setProjection(float fov, float aspect_ratio, float near, float far)
   float top = tan(glm::radians(fov / 2)) * near;
   float bottom = -top;
   float right = top * aspect_ratio;
-  float left = bottom;
+  float left = -right;
   projection = glm::mat4(2 * near / (right - left), 0, 0, 0,
                          0, 2 * near / (top - bottom), 0, 0,
                          (right + left) / (right - left), (top + bottom) / (top - bottom), -(far + near) / (far - near), -1,
