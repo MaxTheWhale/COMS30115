bbf87e19997bed6240ec3cf85220419f2b58fd76
TobleroneSwordfish <jonathan.hall859@gmail.com>
2020-05-13 17:14:34 +0100

    I swear to god if this fixes it

--- simon/3D/Magnet.cpp
+++ simon/3D/Magnet.cpp
@@ -12,14 +12,14 @@ Magnet::Magnet(Transformable* center) {
 
 void Magnet::update() {
     for (unsigned int i = 0; i < Rigidbody::allRBs.size(); i++) {
-        Rigidbody* rb = Rigidbody::allRBs[i];
-        if (!rb->suckable || rb->model->transform == this->center->transform) { 
+        // Rigidbody* rb = Rigidbody::allRBs[i];
+        if (!Rigidbody::allRBs[i]->suckable || Rigidbody::allRBs[i]->model->transform == this->center->transform) { 
             continue;
         }
-        vec3 dist = center->getPosition() - rb->model->getPosition();
+        vec3 dist = center->getPosition() - Rigidbody::allRBs[i]->model->getPosition();
         vec3 force = timeStep() * attractionStrength * dist / (length(dist) * length(dist));
         // cout << "force = " << force << endl;
         totalForce += force;
-        rb->applyForce(force, vec3(0,0,0));
+        Rigidbody::allRBs[i]->applyForce(force, vec3(0,0,0));
     }
 }
